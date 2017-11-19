b5a1fa847eb22e0cece4af03f547893d6b685cfa
Roman Parise <pariseroman@gmail.com>
2017-11-18 16:41:14 -0800

    Updated with mosfet tables and figs

--- Lab6/data/test.py
+++ Lab6/data/test.py
@@ -1,5 +1,36 @@
+#!/usr/bin/python
+
+from common import *
+import matplotlib.pyplot as plt
+
+TABLES_DIR = "../tables/"
+IMAGES_DIR = "../images/"
+CSV_EXT = ".csv"
+PNG_EXT = ".PNG"
+
+PREC = 3
+R1 = 0.998e3
+R2 = 9.902e3
+
 # MOSFET Circuit 1
+mosfet_circuit_1_fname = "mosfet_id_vgs"
+mosfet_circuit_1_data_fname = TABLES_DIR + mosfet_circuit_1_fname + CSV_EXT
+mosfet_circuit_1_fig_fname = IMAGES_DIR + mosfet_circuit_1_fname + PNG_EXT
 V2 = 10
-V1 = [ 0 , 0.5 , 1 , 1.5 , 2 , 2.05 , 2.10 , 2.15 , 2.20 , 2.25 , 2.30 , 2.35 , 2.40 , 2.45 , 2.5 , 3 , 5 ] # 0 - 5V
+V1 = [ 0 , 0.5 , 1 , 1.5 , 2 , 2.05 , 2.10 , 2.15 , 2.20 , 2.25 , 2.30 , 2.35 , 2.40 , 2.45 , 2.5 , 3 , 3.5 , 4 , 4.5 , 5 ] # 0 - 5V
 Vr2 = [ 0 , 0 , 0 , 0 , 0.064 , 0.127 , 0.225 , 0.418 , 0.702 , 1.211 , 1.885 , 3.025, 4.314 , 6.309 , 8.172 , 9.965 , 9.978 , 9.983 , 9.985 , 9.987 ]
+I_D = [ 1e3 * ( data_point / R2 ) for data_point in Vr2 ]
 Vgs = [ 0 , 0.500 , 0.999 , 1.500 , 1.999 , 2.05 , 2.100 , 2.150 , 2.20 , 2.25 , 2.30 , 2.35 , 2.40 , 2.45 , 2.498 , 2.998 , 3.497 , 3.996 , 4.497 , 4.996 ]
+
+plt.plot( Vgs , I_D )
+plt.xlabel( "$V_{GS}$ [ V ]" )
+plt.ylabel( "$I_D$ [ mA ]" )
+plt.savefig( mosfet_circuit_1_fig_fname )
+
+data_matrix_mosfet_1 = []
+for row_count in range( 0 , len(  V1 ) ) :
+	row = [ V1[ row_count ] , Vr2[ row_count ] , I_D[ row_count ] , Vgs[ row_count ] ]
+	data_matrix_mosfet_1.append( [ set_precision_str( data_point , PREC ) for data_point in row ] )
+data_matrix_mosfet_1_headings = [ "$V_1$ [ V ]" , "$V_{R2}$ [ V ]" , "$I_D$ [ mA ]" , "$V_{GS}$ [ V ]" ]
+data_matrix_mosfet_1 = [ data_matrix_mosfet_1_headings ] + data_matrix_mosfet_1
+write_csv_from_matrix( mosfet_circuit_1_data_fname , data_matrix_mosfet_1 )
--- common/scripts/build/lib.linux-x86_64-2.7/common.py
+++ common/scripts/build/lib.linux-x86_64-2.7/common.py
@@ -5,6 +5,7 @@
 WRITE="w"
 COMMA_DELIMITER=","
 NEWLINE="\n"
+CSV_EXT=".csv"
 
 def perc_err( measured , theoretical ):
 	return abs( ( measured - theoretical ) / theoretical )
@@ -13,8 +14,8 @@ def fmt_perc_err( measured , theoretical ) :
 	return fmt_perc( perc_err( measured , theoretical ) )
 
 # TODO: Make the precision on these variable
-def set_precision_str( fp_number ):
-	return ( "%.1f" % fp_number )
+def set_precision_str( fp_number , prec ):
+	return ( ( "%." + str( prec ) + "f" ) % fp_number )
 
 def fmt_perc( perc_as_decimal ):
 	return ( set_precision_str( 100 * perc_as_decimal ) ) + "\%"
@@ -22,7 +23,7 @@ def fmt_perc( perc_as_decimal ):
 # TODO: Overwrites files. Is this the best way to do it?
 # TODO: Error checking?
 def write_csv_from_matrix( fname , matrix ):
-	with open( fname , "w" ) as file_handle:
+	with open( fname , WRITE ) as file_handle:
 		for row in matrix:
 			str_to_write = COMMA_DELIMITER.join( [ str( element ) for element in row ] )
 			file_handle.write( str_to_write + NEWLINE )
