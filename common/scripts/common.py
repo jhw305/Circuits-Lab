79e7b3f00f9fa0ebd4300d522dcfc35a1708a7d5
Roman Parise <pariseroman@gmail.com>
2017-11-19 20:07:54 -0800

    Temporary plotting test code for lab 7.

--- common/scripts/common.py
+++ common/scripts/common.py
@@ -2,6 +2,9 @@
 
 # Roman Parise
 # Common functions for data processing
+import csv
+import matplotlib.pyplot as plt
+
 WRITE="w"
 COMMA_DELIMITER=","
 NEWLINE="\n"
@@ -13,7 +16,6 @@ def perc_err( measured , theoretical ):
 def fmt_perc_err( measured , theoretical ) :
 	return fmt_perc( perc_err( measured , theoretical ) )
 
-# TODO: Make the precision on these variable
 def set_precision_str( fp_number , prec ):
 	return ( ( "%." + str( prec ) + "f" ) % fp_number )
 
@@ -27,3 +29,20 @@ def write_csv_from_matrix( fname , matrix ):
 		for row in matrix:
 			str_to_write = COMMA_DELIMITER.join( [ str( element ) for element in row ] )
 			file_handle.write( str_to_write + NEWLINE )
+
+# TODO: Needs error checking
+# Reads out a csv file into rows
+def read_csv_rows( csv_fname ) :
+	with open( csv_fname , "rb" ) as csv_file :
+		reader = csv.reader( csv_file , delimiter = COMMA_DELIMITER )
+		return [ [ float( element ) for element in row ] for row in reader ]
+
+def save_plot_xy( fname , x_list , y_list ) :
+	plt.plot( x_list , y_list )
+	plt.savefig( fname )
+	plt.clf( )
+
+if __name__ == "__main__" :
+	rows = read_csv_rows( "test.csv" )
+	print( rows )
+	save_plot_xy( "test_file.png" , rows[ 0 ] , rows[ 1 ] )
