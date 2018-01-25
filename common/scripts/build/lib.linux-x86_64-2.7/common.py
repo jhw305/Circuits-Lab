1d7b69b2e48994a2ce101b4ef079d5b29192f061
Roman Parise <pariseroman@gmail.com>
2018-01-25 10:15:54 -0800

    data for lab 2

--- common/scripts/build/lib.linux-x86_64-2.7/common.py
+++ common/scripts/build/lib.linux-x86_64-2.7/common.py
@@ -1,19 +1,43 @@
 #!/usr/bin/python
 
 # Roman Parise
-# Common functions for data processing
+# Common functions for melatonin flow
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import stats
+
 WRITE="w"
 COMMA_DELIMITER=","
 NEWLINE="\n"
 CSV_EXT=".csv"
+TEX_EXT=".tex"
+WRAP="_wrap"
 
+# File generation
+def wrap_fname( section_name ) :
+	return section_name + WRAP + TEX_EXT
+
+def content_fname( section_name ) :
+	return section_name + TEX_EXT
+
+def gen_makefile_txt( section_name ) :
+	txt = ""
+	txt += "LATEX=pdflatex" + NEWLINE
+	txt += "BUILD=$(LATEX) " + wrap_fname( section_name ) + NEWLINE
+	txt += NEWLINE
+	txt += "build: " + content_fname( section_name ) + " " + wrap_fname( section_name ) + NEWLINE
+	txt += "	$(BUILD)" + NEWLINE
+	txt += "	$(BUILD)" + NEWLINE
+	return txt
+
+# Data processing
 def perc_err( measured , theoretical ):
 	return abs( ( measured - theoretical ) / theoretical )
 
 def fmt_perc_err( measured , theoretical ) :
 	return fmt_perc( perc_err( measured , theoretical ) )
 
-# TODO: Make the precision on these variable
 def set_precision_str( fp_number , prec ):
 	return ( ( "%." + str( prec ) + "f" ) % fp_number )
 
@@ -27,3 +51,29 @@ def write_csv_from_matrix( fname , matrix ):
 		for row in matrix:
 			str_to_write = COMMA_DELIMITER.join( [ str( element ) for element in row ] )
 			file_handle.write( str_to_write + NEWLINE )
+
+# TODO: Needs error checking
+# Reads out a csv file into rows
+def read_csv_rows( csv_fname ) :
+	with open( csv_fname , "rb" ) as csv_file :
+		reader = csv.reader( csv_file , delimiter = COMMA_DELIMITER )
+		return [ [ float( element ) for element in row ] for row in reader ]
+
+def save_plot_xy( fname , x_list , y_list ) :
+	plt.plot( x_list , y_list )
+	plt.savefig( fname )
+	plt.clf( )
+
+def save_plot_xy_with_best_fit_line( fname , x_list , y_list ) :
+	plt.plot( x_list , y_list )
+	m , b , r , p , e = stats.linregress( x_list , y_list )
+	x = np.linspace( min( x_list ) , max( x_list ) , 100 * len( x_list ) )
+	y = m * x  + b
+	plt.plot( x , y )
+	plt.savefig( fname )
+	plt.clf( )
+
+if __name__ == "__main__" :
+	rows = read_csv_rows( "test.csv" )
+	print( rows )
+	save_plot_xy_with_best_fit_line( "test_file.png" , rows[ 0 ] , rows[ 1 ] )
